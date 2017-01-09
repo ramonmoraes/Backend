adb1194f0588f846968c636880d9c6ed7ffda076
ramonmoraes <ramon.moraes94@gmail.com>
2017-01-09 19:55:27 -0300

    Mais organizado *BUG
    BUG: Se derrubar o servidor, re-upalo e fizer um novo cadastro
    diretamente de /cadastro, os dados cadastrados irão sobreescrever todo
    os posteriores.
    -Add novas features no html view

--- myapp/bancodados.js
+++ myapp/bancodados.js
@@ -1,1 +1,1 @@
-[{"nome":"a","sobrenome":"b"},{"nome":"Ola","sobrenome":"eoq"},{"nome":"Tex","sobrenome":"t"},{"nome":"ad","sobrenome":"ad"},{"nome":"a","sobrenome":"b"},{"nome":"a","sobrenome":"b"}]
+[{"nome":"kebab","sobrenome":"kebab"}]
--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -3,11 +3,26 @@ var router = express.Router();
 var pessoas = [];
 var arquivo="E:/Programação/Node/MyFit/myapp/bancodados.js";
 
+var ler_arquivo = function(callback){
+  var fs = require("fs");
+  fs.readFile(arquivo, callback);
+}
+
+var escrever_arquivo = function(hash){
+  var fs = require("fs");
+  
+  pessoas.push(hash); //Nao adiantou nada, se der F5 no /cadastro logo apos upar o server, o banco sera overwritten;
+  fs.writeFile(arquivo, JSON.stringify(pessoas), function(err){
+  if(err){
+      console.log("err");
+        }
+      });
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-    var fs = require("fs");
-    fs.readFile(arquivo, function read(err, data){
+ler_arquivo(function read(err, data){
       if (err) {
         console.log("nao ta lendo essa merda");
         res.render('index', { title: 'Express' , pessoas:[] });
@@ -25,23 +40,14 @@ router.post('/cadastro', function(req, res, next) {
 if((req.body.firstname=='')||(req.body.lastname=='')){
   return false;
 }else{
-data = {
+var hash = {
     nome: req.body.firstname,
     sobrenome: req.body.lastname
+       }
 }
-}
 
-pessoas.push(data);
-
-var fs = require('fs');
-fs.writeFile(arquivo, JSON.stringify(pessoas), function(err){
-if(err){
-    console.log("err");
-      }
-});
-
-console.log(pessoas);
-  res.render('index', { title: 'Entrou no cadastro' , pessoas:pessoas });
+escrever_arquivo(hash);
+res.render('index', { title: 'Entrou no cadastro' , pessoas:pessoas });
 });
 /* Cadastro          /\ */
 module.exports = router;
